892c58d09b4de9883d460be7c36be61f97b8dfba
Sri-Aryan <aryansri.official@gmail.com>
2023-10-14 00:38:18 +0530

    Adding Yt Link for Sign Language

--- app/src/main/java/com/example/imagepro/MainActivity.java
+++ app/src/main/java/com/example/imagepro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.imagepro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,12 +24,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Button camera_button;
+    private Button learn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
                                                                                 
+        learn =findViewById(R.id.learnbutton);
+        learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=YgPYFmM4Sl0&ab_channel=TheSpecialGurujiOfficial"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.google.android.youtube");
+                startActivity(intent);
+            }
+        });
 
         // select device and run
         // we successfully loaded model
